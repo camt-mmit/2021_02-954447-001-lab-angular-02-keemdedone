69fada9be3b8d1374bc287916d8150bb3a0ff359
keemdedone <komchan447@gmail.com>
2021-12-26 09:32:32 +0700

    commit msg

--- src/app/dynamic-input/dynamic-input.component.ts
+++ src/app/dynamic-input/dynamic-input.component.ts
@@ -1,16 +1,18 @@
-import { Content } from '@angular/compiler/src/render3/r3_ast';
-import { Component, OnInit } from '@angular/core';
+import { Content, Template } from '@angular/compiler/src/render3/r3_ast';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dynamic-input',
   templateUrl: './dynamic-input.component.html',
-  styleUrls: ['./dynamic-input.component.scss']
+  styleUrls: ['./dynamic-input.component.scss'],
+  template: ``
 })
+
+
 export class DynamicInputComponent implements OnInit {
 
   inputs =[{value:0}];
-  sections = [{}];
-
+  sections = [{section:this.inputs}];
 
   ngOnInit(): void {
     return;
@@ -18,6 +20,12 @@ export class DynamicInputComponent implements OnInit {
 
   onChange(index:number,value:number): void {
     this.inputs[index].value = value;
+  }
+
+  getResult(): string{
+    let total = 0;
+    this.inputs.forEach((num) => total += num.value);
+    return total.toString();
   }
 
   addInput(): void{
@@ -28,14 +36,9 @@ export class DynamicInputComponent implements OnInit {
     this.inputs.splice(index,1);
   }
 
-  getResult(): string{
-    let total = 0;
-    this.inputs.forEach((num) => total += num.value);
-    return total.toString();
-  }
-
   addSection(): void{
-    this.sections.push({});
+    this.sections.push({section:this.inputs});
+    console.log(this.sections);
   }
 
   deleteSection(index:number): void{
